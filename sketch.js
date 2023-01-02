affd7419d8c2e8f27eec14f67da422c7495cdcf7
SHNNN22 <45427181+SHNNN22@users.noreply.github.com>
2023-01-02 12:42:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,129 +1,141 @@
-/*
-var img = [];
-var index = 0;
-var width = 500;
-function preload(){
+let imageRatio, //ratio of the image h/w
+  index = 0,
+  canvas,
+  moon = [], //image
+  moonCopy = [], //resized image of the image (visible)
+  moonOri = [],
+  allPics = 0;
 
-  img[0] = loadImage("pics/pic0.jpg");
-	img[1] = loadImage("pics/pic1.jpg");
-	img[2] = loadImage("pics/pic2.jpg");
-	img[3] = loadImage("pics/pic3.jpg");
-	img[4] = loadImage("pics/pic4.jpg");
-}
-
-function setup(){
-  createCanvas(windowWidth,windowHeight);
-}
-
-function draw(){
-  image(img[index],0,0);
-}
-function windowResized() {
-   resizeCanvas(windowWidth, windowHeight);
-}
-
-function mouseReleased(){
-
-    //ONLY if click registered inside image
-    if(mouseX>width/2){
-          index = index + 1;
-          if (index == img.length){
-              index = index - img.length;
-          }
-    } else if(index>0){
-			index = index - 1;
-			if (index == img.length){
-					index = index - img.length;
-			}
-		}
-}
-
-function mouseHover(){
-
-    //ONLY if click registered inside image
-    if(mouseX>width && mouseY>height){
-			image(img[index+1],50,50,width,windowHeight);
-}
-}
-*/
-
-var moon = [],       //image
-    moonCopy = [],        //resized image of the image (visible)
-    imageRatio;     //ratio of the image h/w
-
-		var index = 0;
-var canvas
-let ori = []
 function preload() {
-	moon[0]=loadImage("pics/pic0.jpg", img => moonCopy[0] = img.get());
-	moon[1]=loadImage("pics/pic1.jpg", img => moonCopy[1] = img.get());
-	moon[2]=loadImage("pics/pic2.jpg", img => moonCopy[2] = img.get());
-	moon[3]=loadImage("pics/pic3.jpg", img => moonCopy[3] = img.get());
-  moon[4]=loadImage("pics/pic4.jpg", img => moonCopy[4] = img.get());
-  moon[5]=loadImage("pics/pic5.jpg", img => moonCopy[5] = img.get());
-  moon[6]=loadImage("pics/pic6.jpg", img => moonCopy[6] = img.get());
-  moon[7]=loadImage("pics/pic7.jpg", img => moonCopy[7] = img.get());
-  moon[8]=loadImage("pics/pic8.jpg", img => moonCopy[8] = img.get());
-  moon[9]=loadImage("pics/pic9.jpg", img => moonCopy[9] = img.get());
-		//img[0] = loadImage("pics/pic0.jpg");
-		ori[0]=loadImage("pics/pic0ori.jpg")
-		ori[1]=loadImage("pics/pic1ori.jpg")
-		ori[2]=loadImage("pics/pic2ori.jpg")
-		ori[3]=loadImage("pics/pic3ori.jpg")
-		ori[4]=loadImage("pics/pic4ori.jpg")
-    ori[5]=loadImage("pics/pic5ori.jpg")
-    ori[6]=loadImage("pics/pic6ori.jpg")
-    ori[7]=loadImage("pics/pic7ori.jpg")
-    ori[8]=loadImage("pics/pic8ori.jpg")
-    ori[9]=loadImage("pics/pic9ori.jpg")
+  if (document.title == "Beauty") {
+    allPics = 10;
+  } else if (document.title == "Stills") {
+    allPics = 10;
+  } else if (document.title == "Add") {
+    allPics = 12;
+  } else if (document.title == "Remove") {
+    allPics = 13;
+  } else if (document.title == "Background") {
+    allPics = 8;
+  } else if (document.title == "Set") {
+    allPics = 10;
+  } else if (document.title == "Reflections") {
+    allPics = 6;
+  } else if (document.title == "Wrinkles") {
+    allPics = 9;
+  } else if (document.title == "Apply") {
+    allPics = 13;
+  } else if (document.title == "Adjustments") {
+    allPics = 14;
+  } else if (document.title == "Color Correction") {
+    allPics = 10;
+  } else if (document.title == "Color Change") {
+    allPics = 13;
+  } else if (document.title == "Sharpen") {
+    allPics = 6;
+  } else if (document.title == "Selections") {
+    allPics = 4;
+  } else if (document.title == "Themed") {
+    allPics = 8;
+  } else if (document.title == "Kids") {
+    allPics = 9;
+  } else if (document.title == "Jewellery") {
+    allPics = 4;
+  }
+  for (let i = 0; i < allPics; i++) {
+    moon[i] = loadImage(
+      `pics/${document.title}/pic${i}.jpg`,
+      (img) => (moonCopy[i] = img.get())
+    );
+    moonOri[i] = loadImage(`pics/${document.title}/pic${i}ori.jpg`);
+  }
 }
 
-function setup()   {
-    canvas = createCanvas(windowWidth, windowHeight);
-
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(255);
-    if(width<height){
-    imageRatio = moon[index].width/moon[index].height;
-    } else {
-    imageRatio = moon[index].height/moon[index].width;
-    }
-      image(moonCopy[index],width/2 - (windowHeight/imageRatio)/2,0,windowHeight/imageRatio, windowHeight);
+  background(255);
+  document.getElementById("page").innerText = index + 1 + " /" + allPics;
 
-      if(mouseX>width/2 - (windowHeight/imageRatio)/2 && mouseX<width/2 + (windowHeight/imageRatio)/2) {
-        hover()
-      }
+  if (width < height) {
+    imageRatio = moon[index].width / moon[index].height;
+  } else {
+    imageRatio = moon[index].height / moon[index].width;
+  }
 
+  image(
+    moonCopy[index],
+    width / 2 - windowHeight / imageRatio / 2,
+    0,
+    windowHeight / imageRatio,
+    windowHeight
+  );
+
+  if (
+    mouseX > width / 2 - windowHeight / imageRatio / 2 &&
+    mouseX < width / 2 + windowHeight / imageRatio / 2
+  ) {
+    hover();
+  }
+
+  if (mouseX > windowWidth / 2 + windowHeight / imageRatio / 2) {
+    cursor("/pics/LAYOUT/RIGHT.png");
+  } else if (
+    index > 0 &&
+    mouseX < windowWidth / 2 - windowHeight / imageRatio / 2
+  ) {
+    cursor("/pics/LAYOUT/LEFT.png");
+  } else {
+    cursor(ARROW);
+  }
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    print("resizing to: "+windowWidth+" "+windowHeight);
-    if (windowHeight < moon[index].height){
-        moonCopy[index] = moon[index].get();
-        moonCopy[index].resize(0, windowHeight);
-    }
+  resizeCanvas(windowWidth, windowHeight);
+  // print("resizing to: "+windowWidth+" "+windowHeight);
+  if (windowHeight < moon[index].height) {
+    moonCopy[index] = moon[index].get();
+    moonCopy[index].resize(0, windowHeight);
+  }
 }
 
-function mouseReleased(){
+function mouseReleased() {
+  //ONLY if click registered inside image
+  if (mouseX > width / 2 + windowHeight / imageRatio / 2) {
+    index = index + 1;
+    if (index == moon.length) {
+      index = index - moon.length;
+    }
+  } else if (index > 0 && mouseX < width / 2 - windowHeight / imageRatio / 2) {
+    index = index - 1;
+    if (index == moon.length) {
+      index = index - moon.length;
+    }
+  }
+}
 
-    //ONLY if click registered inside image
-    if(mouseX>width/2 + (windowHeight/imageRatio)/2){
-          index = index + 1;
-          if (index == moon.length){
-              index = index - moon.length;
-          }
-    } else if(index>0 && mouseX<width/2 - (windowHeight/imageRatio)/2){
-			index = index - 1;
-			if (index == moon.length){
-					index = index - moon.length;
-			}
-		}
+function keyPressed() {
+  if (index > 0 && keyCode === LEFT_ARROW) {
+    index = index - 1;
+    if (index == moon.length) {
+      index = index - moon.length;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    index = index + 1;
+    if (index == moon.length) {
+      index = index - moon.length;
+    }
+  }
 }
 
 function hover() {
-		image(ori[index],width/2 - (windowHeight/imageRatio)/2,0,windowHeight/imageRatio, windowHeight);
-
+  image(
+    moonOri[index],
+    width / 2 - windowHeight / imageRatio / 2,
+    0,
+    windowHeight / imageRatio,
+    windowHeight
+  );
 }
